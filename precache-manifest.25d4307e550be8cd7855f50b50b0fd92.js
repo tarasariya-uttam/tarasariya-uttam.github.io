ff5df79a58d147324b2621c20a3e55369ef65a2f
uttam <uttam@wolfpointagency.com>
2021-05-02 09:02:54 +0530

    Updates

---
+++ precache-manifest.25d4307e550be8cd7855f50b50b0fd92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7d2b7ab8ba9dce8dcaaab7813e24f7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa5ee38f9d2df13c06d1",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa5ee38f9d2df13c06d1",
+    "url": "/static/js/main.508344cc.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tarasariya-uttam.github.io/precache-manifest.f5c435c19fcac0fa147894b8adbb268f.js"
+  "/precache-manifest.25d4307e550be8cd7855f50b50b0fd92.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tarasariya-uttam.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
